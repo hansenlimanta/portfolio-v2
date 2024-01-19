d1ddd26dda4517b9aa4ec2082090b2e00be74be6
hansen.midas <hansen.limanta@midasteknologi.com>
2024-01-19 16:54:23 +0700

    feat: add projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import {
   AiOutlineMail,
 } from "react-icons/ai";
 import { MdArrowOutward } from "react-icons/md";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
@@ -43,7 +44,7 @@ export default function HomePage() {
           </ul>
         </header>
         <main className="flex w-1/2 flex-col gap-4 py-20 ">
-          <div className="mb-40 flex flex-col gap-3 px-4" id="about">
+          <section className="mb-32 flex flex-col gap-3 px-4" id="about">
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Necessitatibus consequuntur enim, provident, voluptate
@@ -63,84 +64,177 @@ export default function HomePage() {
               repellat ducimus dignissimos aliquam rerum delectus odio minima?
               Minima, animi perferendis!
             </p>
-          </div>
-          <div
-            className="group/parent flex flex-col items-center justify-center gap-2 transition-all"
+          </section>
+          <section
+            className="mb-32 flex flex-col items-start justify-center gap-2"
             id="experience"
           >
-            <div className="group/exp flex gap-4 rounded-lg p-4 opacity-100 transition-all hover:cursor-pointer hover:bg-slate-800 hover:!opacity-100 group-hover/parent:opacity-50">
-              <p className="mt-1 flex-1 text-sm">2023 — Present</p>
-              <div className="flex flex-[3] flex-col gap-2">
-                <h2 className="font-semibold text-slate-200 transition-colors group-hover/exp:text-teal-300">
-                  Fullstack Developer · Midas Daya Teknologi
-                </h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Dolorum facere optio, eum amet beatae placeat nobis id. Neque,
-                  asperiores veniam? Fugit odit qui voluptatibus repellendus non
-                  dolores rerum autem sequi?
-                </p>
-                <ul className="flex flex-wrap gap-2 ">
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Vue
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Vuetify
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Java
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Springboot
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Vuex
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Git
-                  </li>
-                </ul>
+            <div className="group/parent flex flex-col items-center justify-center gap-2 transition-all">
+              <div className="group/exp flex gap-4 rounded-lg p-4 opacity-100 transition-all hover:cursor-pointer hover:bg-slate-800 hover:!opacity-100 hover:shadow-lg group-hover/parent:opacity-50">
+                <p className="mt-1 flex-1 text-sm">2023 — Present</p>
+                <div className="flex flex-[3] flex-col gap-2">
+                  <h2 className="font-semibold text-slate-200 transition-colors group-hover/exp:text-teal-300">
+                    Fullstack Developer · Midas Daya Teknologi
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolorum facere optio, eum amet beatae placeat nobis id.
+                    Neque, asperiores veniam? Fugit odit qui voluptatibus
+                    repellendus non dolores rerum autem sequi?
+                  </p>
+                  <ul className="flex flex-wrap gap-2 ">
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Vue
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Vuetify
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Java
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Springboot
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Vuex
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Git
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="group/exp flex gap-4 rounded-lg p-4 opacity-100 transition-all hover:cursor-pointer hover:bg-slate-800 hover:!opacity-100 group-hover/parent:opacity-50">
+                <p className="mt-1 flex-1 text-sm">2022 — 2023</p>
+                <div className="flex flex-[3] flex-col gap-2">
+                  <h2 className="font-semibold text-slate-200 transition-colors group-hover:text-teal-300">
+                    Frontend Developer · Sagara Technology
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolorum facere optio, eum amet beatae placeat nobis id.
+                    Neque, asperiores veniam? Fugit odit qui voluptatibus
+                    repellendus non dolores rerum autem sequi?
+                  </p>
+                  <ul className="flex flex-wrap gap-2 ">
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      React
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      MUI
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      React Hook Form
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      NextJS
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Styled Component
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Figma
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="group/exp flex gap-4 rounded-lg p-4 opacity-100 transition-all hover:cursor-pointer hover:bg-slate-800 hover:!opacity-100 group-hover/parent:opacity-50">
-              <p className="mt-1 flex-1 text-sm">2022 — 2023</p>
-              <div className="flex flex-[3] flex-col gap-2">
-                <h2 className="font-semibold text-slate-200 transition-colors group-hover:text-teal-300">
-                  Frontend Developer · Sagara Technology
-                </h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Dolorum facere optio, eum amet beatae placeat nobis id. Neque,
-                  asperiores veniam? Fugit odit qui voluptatibus repellendus non
-                  dolores rerum autem sequi?
-                </p>
-                <ul className="flex flex-wrap gap-2 ">
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    React
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    MUI
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    React Hook Form
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    NextJS
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Styled Component
-                  </li>
-                  <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
-                    Figma
-                  </li>
-                </ul>
+            <div className="mt-8 inline-flex w-fit cursor-pointer items-center justify-start gap-2 px-4 font-semibold text-slate-200 transition-colors hover:text-teal-300">
+              View Full Resume <MdArrowOutward size={20} />
+            </div>
+          </section>
+          <section
+            className="mb-32 flex flex-col items-start justify-center gap-2"
+            id="projects"
+          >
+            <div className="group/parent flex flex-col items-center justify-center gap-2 transition-all">
+              <div className="group/exp flex gap-4 rounded-lg p-4 opacity-100 transition-all hover:cursor-pointer hover:bg-slate-800 hover:!opacity-100 hover:shadow-lg group-hover/parent:opacity-50">
+                <div className="relative mt-1 h-1/3 w-full flex-1 overflow-hidden rounded border-2 border-slate-700 group-hover/exp:border-slate-400">
+                  <Image
+                    src={"/projects/notes1.jpg"}
+                    alt="personal notes"
+                    objectFit="cover"
+                    fill
+                  />
+                </div>
+                <div className="flex flex-[3] flex-col gap-2">
+                  <h2 className="font-semibold text-slate-200 transition-colors group-hover/exp:text-teal-300">
+                    Fullstack Developer · Midas Daya Teknologi
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolorum facere optio, eum amet beatae placeat nobis id.
+                    Neque, asperiores veniam? Fugit odit qui voluptatibus
+                    repellendus non dolores rerum autem sequi?
+                  </p>
+                  <ul className="flex flex-wrap gap-2 ">
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Vue
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Vuetify
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Java
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Springboot
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Vuex
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Git
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="group/exp flex gap-4 rounded-lg p-4 opacity-100 transition-all hover:cursor-pointer hover:bg-slate-800 hover:!opacity-100 group-hover/parent:opacity-50">
+                <div className="relative mt-1 h-1/3 w-full flex-1 overflow-hidden rounded border-2 border-slate-700 group-hover/exp:border-slate-400">
+                  <Image
+                    src={"/projects/music1.jpg"}
+                    alt="music player"
+                    objectFit="cover"
+                    fill
+                  />
+                </div>
+                <div className="flex flex-[3] flex-col gap-2">
+                  <h2 className="font-semibold text-slate-200 transition-colors group-hover:text-teal-300">
+                    Frontend Developer · Sagara Technology
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolorum facere optio, eum amet beatae placeat nobis id.
+                    Neque, asperiores veniam? Fugit odit qui voluptatibus
+                    repellendus non dolores rerum autem sequi?
+                  </p>
+                  <ul className="flex flex-wrap gap-2 ">
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      React
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      MUI
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      React Hook Form
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      NextJS
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Styled Component
+                    </li>
+                    <li className="rounded-full bg-teal-400/10 px-3 py-1 text-xs text-teal-300">
+                      Figma
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="inline-flex w-fit cursor-pointer items-center justify-start gap-2 px-4 font-semibold text-slate-200 transition-colors hover:text-teal-300">
-            View Full Resume <MdArrowOutward size={20} />
-          </div>
-          <div className="flex flex-col" id="projects"></div>
+            <div className="mt-8 inline-flex w-fit cursor-pointer items-center justify-start gap-2 px-4 font-semibold text-slate-200 transition-colors hover:text-teal-300">
+              View Full Project Archive <MdArrowOutward size={20} />
+            </div>
+          </section>
         </main>
       </div>
     </div>
